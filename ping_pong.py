0065f9a9e9f60aeba800ef3607d068540999992b
Amable-Loca <alvarezainhoa24@gmail.com>
2025-06-14 11:36:59 -0400

    Update ping_pong.py
    Agregada condici9on de victoria P1

--- ping_pong.py
+++ ping_pong.py
@@ -1,14 +1,19 @@
 from pygame import *
 init()
+font.init()
 
 ANCHO, ALTO = 753, 553
 COLOR_FONDO = (179, 224, 230)
+BLANCO = (255, 255, 255)
 FPS = 120
 PLAYER_IMG = 'racket.png'
-PELOTA ='pelota.png'
+PELOTA = 'pelota.png'
+FUENTE = 'Pixelcraft.ttf'
 
 screen = display.set_mode((ANCHO, ALTO))
-display.set_caption('PING_PONG') 
+display.set_caption('PING_PONG')
+
+font_1 = font.Font(FUENTE, 50)
 
 class GameSprite(sprite.Sprite):
     def __init__(self,img ,cor_x, cor_y, width, height , speed=0):
@@ -44,10 +49,12 @@ class Player(GameSprite):
 
 
 player_1 = Player(PLAYER_IMG, 5, 250, 30, 100, 5)
-player_2 = Player(PLAYER_IMG, 715,250,30,100,5  )
-pelota = GameSprite(PELOTA,350,250, 40 ,40 , 10)
+player_2 = Player(PLAYER_IMG, 715, 250, 30, 100, 5)
+pelota = GameSprite(PELOTA, 50, 50, 40, 40, 1)
 
-speed_x, speed_y = 2,2
+speed_x, speed_y = 5, 5
+
+
 clok = time.Clock()
 finish = False
 run = True
@@ -55,8 +62,6 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False 
-
-
 
     if not finish:
         screen.fill(COLOR_FONDO)
@@ -66,19 +71,23 @@ while run:
         player_2.update_2()
         pelota.reset()
 
-        pelota.rect.x +=  speed_x
-        pelota.rect.y +=  speed_y
-   
-    if  pelota.rect.y>=ALTO - pelota.height:
-        speed_y *= -1
-    if  pelota.rect.y <= 0:
-        speed_y *= -1
-    if sprite.collide_rect (pelota,player_1):
-        speed_x *=-1
-    if sprite.collide_rect(pelota,player_2):
-        speed_x *=-1
+        pelota.rect.x += speed_x
+        pelota.rect.y += speed_y
 
-   
+        if pelota.rect.y >= ALTO - pelota.height:
+            speed_y *= -1
+        if pelota.rect.y <= 0:
+            speed_y *= -1
+        if sprite.collide_rect(pelota, player_1):
+            speed_x *= -1
+        if sprite.collide_rect(pelota, player_2):
+            speed_x *= -1
+        
+
+        if pelota.rect.x > ANCHO:
+            win_p1 = font_1.render('GANADOR JUGADOR 1', 1, BLANCO)
+            screen.blit(win_p1, (100, 250))
+            finish = True
    
     display.update()
     clok.tick(FPS)
